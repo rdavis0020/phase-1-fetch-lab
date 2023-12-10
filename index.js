d652a7bd9784fe03a8e5e983b5d2601a6cbb2e1f
Ryan Davis <rdavis0020@gmail.com>
2023-12-10 18:49:29 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  const url = 'https://anapioficeandfire.com/api/books';
+
+  return fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(data => {
+      renderBooks(data);
+    })
+    .catch(error => {
+      console.error('Error fetching books:', error);
+      throw error;
+    });
 }
+
+function renderBooks(books) {
+  var main = document.querySelector('main');
+  books.forEach(function (book) {
+    var h2 = document.createElement('h2');
+    h2.innerHTML = book.name;
+    main.appendChild(h2);
+  });
+}
+
+fetchBooks()
+  .catch(error => console.error('Error rendering books:', error));
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
